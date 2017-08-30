88463b872599828e50583661a98862b13d2cf383
Strategical IT <richard.houghton@dell.com>
2017-08-30 20:02:26 +1000

    More updates

--- pi-light.py
+++ pi-light.py
@@ -13,20 +13,18 @@ def init():
     ADC0832.setup()
 
 def loop():
-    while True:
+    count = 0
+    while count < 100:
         brightness = ADC0832.getResult() - 80
         if brightness < 0:
     			brightness = 0
         if brightness > 100:
     			brightness = 100
 
-		# print 'brightness = %d' % brightness
-		Counter = r.incr('new_counter')
-        brightness_data = 'brightness' + str(Counter)
-        # print "the name of the new key is: " + brightnessdata
-
+		counter = r.incr('new_counter')
+		brightness_data = 'brightness' + str(counter)
 		r.hmset(brightness_data,{'station':station_id,'brightness':brightness})
-        time.sleep(0.5)
+		time.sleep(0.5)
 
 if __name__ == '__main__':
     init()
