25680c5b833f8f39e65ec9330159192ad80ba94f
Strategical IT <richard.houghton@dell.com>
2017-09-07 13:22:08 +0800

    tidy up

--- WUG temp1.py
+++
@@ -1,6 +0,0 @@
-url = "http://api.wunderground.com/api/yourkey/conditions/q/AU/melbourne.json"
-        response = requests.get(url)
-        parsed = json.loads(response.content)
-        weather = parsed["current_observation"]["weather"]
-        temp = parsed["current_observation"]["temp_c"]
-        hum = parsed["current_observation"]["relative_humidity"]
